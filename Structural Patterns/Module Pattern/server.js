f3f4a995b15ae1be49ed34db3c01ef67ef4088a3
Ankur-Marwaha <bhangraburnout@gmail.com>
2019-08-16 10:37:47 +0530

    'asdf'

--- Structural Patterns/Module Pattern/server.js
+++ Structural Patterns/Module Pattern/server.js
@@ -1,7 +1,28 @@
-var http = require('http');
+var express = require("express");
+var app = express();
+var PORT = 3000;
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+app.get("/", function(req, res) {
+  res.status(200).send("asdfs world");
+});
+
+app.get("/asdf", function(req, res) {
+  //res.status(200).send("1/index.html");
+  res.status(200).sendFile("1/index.html", { root: __dirname });
+});
+
+app.listen(PORT, function() {
+  console.log("Server is running on PORT:", PORT);
+});
+
+//---------------------------------
+//CREATING A SERVER WITHOUT EXPRESS
+// var http = require("http");
+
+// //create a server object:
+// http
+//   .createServer(function(req, res) {
+//     res.write("Hello World!"); //write a response to the client
+//     res.end(); //end the response
+//   })
+//   .listen(8080); //the server object listens on port 8080
