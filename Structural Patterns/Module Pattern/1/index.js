e23b38e0032527829c628f287b13e2c4c016e28d
Ankur-Marwaha <bhangraburnout@gmail.com>
2019-08-17 23:43:33 +0530

    'asdf'

--- Structural Patterns/Module Pattern/1/index.js
+++ Structural Patterns/Module Pattern/1/index.js
@@ -1,8 +1,9 @@
-require(["helper/firstModule"], function(util) {
-    alert('index.js');
-    alert(util);
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
-});
+alert(4);
+// require(["helper/firstModule"], function(util) {
+//     alert('index.js');
+//     alert(util);
+//     //This function is called when scripts/helper/util.js is loaded.
+//     //If util.js calls define(), then this function is not fired until
+//     //util's dependencies have loaded, and the util argument will hold
+//     //the module value for "helper/util".
+// });
