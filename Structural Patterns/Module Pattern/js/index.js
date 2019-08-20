85e4657e1d1ce3fecdad454fe5e7aa6ae3578484
Ankur-Marwaha <bhangraburnout@gmail.com>
2019-08-20 22:28:11 +0530

    final commit

--- Structural Patterns/Module Pattern/js/index.js
+++ Structural Patterns/Module Pattern/js/index.js
@@ -1,8 +1,13 @@
-require(["helper/firstModule", "jquery"], function(firstModule, jquery) {
+define("index", ["helper/firstModule", "jquery"], function(
+  firstModule,
+  jquery
+) {
+  //the following doesn't get defined as a module
+  //require(["helper/firstModule", "jquery"], function(firstModule, jquery) {
   alert("index.js");
-  //alert(firstModule);
   firstModule.firstMethod();
   if (jquery) {
     alert("jquery is defined");
   }
+  return 4;
 });
